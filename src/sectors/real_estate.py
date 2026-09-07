1611653df06b1b559896f0bf972268c1a1c96bd2
Abhishek Singh (UIDAI 9414 9122 9013) <abhishek1033@gmail.com>
2026-09-07 14:26:32 +0530

    Create real_estate.py

---
+++ src/sectors/real_estate.py
@@ -0,0 +1,7 @@
+from src.taxonomy import EconomicSector
+
+real_estate = EconomicSector(
+    name="Real Estate Digital Registry",
+    macro_share=0.12,
+    taxonomy_class="CLASS_3_HEAVY_RESOURCE"  # Captured via 4.5% Registry Stamp Duty Rail
+)
