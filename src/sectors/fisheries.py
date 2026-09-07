761fc818a84064ac5a38b7634cb2a5da567b26b9
Abhishek Singh (UIDAI 9414 9122 9013) <abhishek1033@gmail.com>
2026-09-07 14:26:55 +0530

    Create fisheries.py

---
+++ src/sectors/fisheries.py
@@ -0,0 +1,7 @@
+from src.taxonomy import EconomicSector
+
+fisheries = EconomicSector(
+    name="Fisheries & Marine Processing",
+    macro_share=0.03,
+    taxonomy_class="CLASS_1_PERISHABLE_AGRI"  # Captured via 3.0% Processing Gate (Artisanal 0%)
+)
