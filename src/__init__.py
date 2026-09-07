6750407bb758da91e761119073bdb3c855920832
Abhishek Singh (UIDAI 9414 9122 9013) <abhishek1033@gmail.com>
2026-09-07 14:24:06 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+V1.4 Macroeconomic Engine Package.
+Provides core simulation logic and universal taxonomy classification.
+"""
+
+from src.taxonomy import EconomicSector
+from src.engine import V14MasterEconomyModel
+
+__all__ = ["EconomicSector", "V14MasterEconomyModel"]
