9c309a3b3b16a8687758d7ffe2649bebfe8f35de
Abhishek Singh (UIDAI 9414 9122 9013) <abhishek1033@gmail.com>
2026-09-07 14:25:59 +0530

    Create energy.py

---
+++ src/sectors/energy.py
@@ -0,0 +1,7 @@
+from src.taxonomy import EconomicSector
+
+energy_fuel = EconomicSector(
+    name="Energy & Bulk Fuel",
+    macro_share=0.15,
+    taxonomy_class="CLASS_3_HEAVY_RESOURCE"  # Captured via 6.0% Refinery & Grid Gate
+)
