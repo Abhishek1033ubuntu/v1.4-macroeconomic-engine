8a9d13edd4f2f8219302be8cfdd023109b5aaa02
Abhishek Singh (UIDAI 9414 9122 9013) <abhishek1033@gmail.com>
2026-09-07 14:25:34 +0530

    Create excise.py

---
+++ src/sectors/excise.py
@@ -0,0 +1,7 @@
+from src.taxonomy import EconomicSector
+
+liquor_excise = EconomicSector(
+    name="High-Excise Liquor",
+    macro_share=0.08,
+    taxonomy_class="CLASS_2_HIGH_EXCISE"  # Captured via 50% Distillery/Depot Gate
+)
